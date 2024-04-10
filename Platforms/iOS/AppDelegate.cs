0fda7896e0b6b63b439b727e45d12f281bf91a99
张祖全 <zhangsir1984@qq.com>
2024-04-10 13:50:10 +0800

    init

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiHttpTest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
